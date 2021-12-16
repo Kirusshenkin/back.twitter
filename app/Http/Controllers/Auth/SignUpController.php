4b513a1bd9f30989b26f2608818641e9f9206def
Kirill <kirill.mitin34@gmail.com>
2021-12-16 18:38:23 +0300

    valitade

--- app/Http/Controllers/Auth/SignUpController.php
+++ app/Http/Controllers/Auth/SignUpController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SignUpRequest;
 use App\Models\User;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class SignUpController extends Controller
 {
-    public function __invoke(Request $request) {
+    public function __invoke(SignUpRequest $request) {
         $user = User::create([
             'email' => $request->get('email'),
             'password' => Hash::make($request->get('password')),
